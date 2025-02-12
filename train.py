338f90a95e6acaa23c6005894609df2cfad11b2c
STGTraffic <180927190@qq.com>
2025-02-12 16:16:42 +0800

    Update train.py

--- train.py
+++ train.py
@@ -125,24 +125,6 @@ def main():
         args.data = "data//" + args.data
         args.num_nodes = 170
 
-    elif args.data == "PEMSBAY":
-        args.data = "data//" + args.data
-        args.num_nodes = 325
-
-    elif args.data == "PEMS04":
-        args.data = "data//" + args.data
-        args.num_nodes = 307
-        
-    elif args.data == "PEMS08_60":
-        args.data = "data//" + args.data
-        args.num_nodes = 170
-        
-    elif args.data == "Urban_60":
-        args.data = "data//" + args.data
-        args.num_nodes = 304
-
-
-
     device = torch.device(args.device)
 
     dataloader = util.load_dataset(
